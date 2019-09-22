e504aa5c068e75684c8a0489cca02f825c34cb36
Pedro Avila <pedro.x.avila.-nd@disney.com>
2019-09-22 08:44:50 -0500

    chore(prisma-client): migrating to prismaclient

---
+++ src/generated/index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Product",
+    embedded: false
+  },
+  {
+    name: "ProductType",
+    embedded: false
+  },
+  {
+    name: "News",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/alejox31-67c020/avilena/avilena-stgf`,
+  secret: `mysecret123`
+});
+exports.prisma = new exports.Prisma();
