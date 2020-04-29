f507bbed47fbf0a9bc8ef2fbe82df5f4cea2c20c
Pedro Avila <pedro.x.avila.-nd@disney.com>
2020-04-29 10:25:57 -0500

    chore(orders): improve orders fetching

--- src/resolvers.js
+++ src/resolvers.js
@@ -5,18 +5,17 @@ const jwt = require('jsonwebtoken');
 
 const sc = simplecrypt();
 
-const asyncForeEach = async(array, callback) => {
-    for (let index = 0; index < (array.length > 7 ? 7:array.length); index++) {
+const asyncForeEach = async (array, callback) => {
+    for (let index = 0; index < (array.length > 7 ? 7 : array.length); index++) {
         await callback(array[index], index, array);
     }
 };
 
-const hashCode = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0);
-
+const hashCode = s => s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0);
 
 const resolvers = {
     Query: {
-        getUser: (parent, { id }, {context}) => {
+        getUser: (parent, {id}, {context}) => {
             return context.prisma.user(
                 {
                     where: {
@@ -25,54 +24,78 @@ const resolvers = {
                 },
             )
         },
-        getStates:async (parent, args, {context}) => {
+        getStates: async (parent, args, {context}) => {
             return await context.prisma.states();
         },
-        getCities:async (parent, {id}, {context}) => {
-            return await context.prisma.cities({where:{state:{id}}});
+        getCities: async (parent, {id}, {context}) => {
+            return await context.prisma.cities({where: {state: {id}}});
         },
-        getProductType:async (parent, { id }, {context}) => {
+        getProductType: async (parent, {id}, {context}) => {
             return await context.prisma.productTypes();
         },
-        getCategoryProducts: async (parent, { id }, {context}) => {
+        getCategoryProducts: async (parent, {id}, {context}) => {
             return await context.prisma.productType({id}).products();
         },
-        currentUser: (parent, { email,password }, context) => {
+        currentUser: (parent, {email, password}, context) => {
             return getUser(context);
         },
-        getOrderDetails: async (parent, { id }, {context}) => {
-            return await context.prisma.order({ id:id }).products()
+        getOrderDetails: async (parent, {id}, {context}) => {
+            return await context.prisma.order({id: id}).products()
         },
-        getOrders: async (parent, { user }, {context}) => {
-            return await context.prisma.user({ id:user }).orders();
+        getOrders: async (parent, {user}, {context}) => {
+            return await context.prisma.user({id: user}).orders();
         },
-        getAllOrders: async (parent, { user }, {context}) => {
+        getAllOrders: async (parent, {user}, {context}) => {
             return await context.prisma.orders();
         },
-        login: async (parent, { email,password }, {context}) => {
-            const user = await context.prisma.users({where:{email}});
+        getAllPendingOrders: async (parent, {user}, {context}) => {
+            return await context.prisma.orders(
+                {
+                    where: {
+                        status: "Esperando Confirmación"
+                    }
+                });
+        },
+        getAllDeliveredOrders: async (parent, {user}, {context}) => {
+            return await context.prisma.orders(
+                {
+                    where: {
+                        status: "Entregada"
+                    }
+                });
+        },
+        getAllOnWayOrders: async (parent, {user}, {context}) => {
+            return await context.prisma.orders(
+                {
+                    where: {
+                        status: "En camino"
+                    }
+                });
+        },
+        login: async (parent, {email, password}, {context}) => {
+            const user = await context.prisma.users({where: {email}});
             if (!user[0]) {
                 throw new Error(`No such user found for email: ${email}`)
             }
             const encriptedPassword = String(hashCode(password));
-            if (encriptedPassword !== user[0].password)  {
+            if (encriptedPassword !== user[0].password) {
                 throw new Error('Contraseña Inválida')
             }
             delete user[0].password;
             const authorization = await jwt.sign(user[0], "secret123");
             return {
                 authorization,
-                user:user[0],
+                user: user[0],
             }
         },
     },
     Mutation: {
-        signup: async (parent, { email, username, phone, address, password, city }, {context}) => {
-            const usr = await context.prisma.users({where:{email}});
+        signup: async (parent, {email, username, phone, address, password, city}, {context}) => {
+            const usr = await context.prisma.users({where: {email}});
             if (usr.length) {
                 throw new Error(`Ya existe un usuario con este correo: ${email}`)
             }
-            const temporaryPassword = password || Math.floor(Math.random()*1000 + 1000).toString();
+            const temporaryPassword = password || Math.floor(Math.random() * 1000 + 1000).toString();
             const pass = String(hashCode(temporaryPassword));
             const user = await context.prisma.createUser(
                 {
@@ -82,18 +105,18 @@ const resolvers = {
                     address,
                     password: pass,
                     city: {
-                        connect:{
+                        connect: {
                             id: city
                         }
                     }
                 });
             delete user.password;
             const authorization = await jwt.sign(user, "secret123");
-            return { authorization, user }
+            return {authorization, user}
         },
-        createOrder: async (parent, { status, products, user }, {context}) => {
+        createOrder: async (parent, {status, products, user}, {context}) => {
             const formattedProducts = products.map((product) => {
-                product.product = {connect:{id:product.product}};
+                product.product = {connect: {id: product.product}};
                 return product;
             });
             return await context.prisma.createOrder({
@@ -106,6 +129,16 @@ const resolvers = {
                 }
             });
         }
+    },
+    Order: {
+        async user(parent,args,{context}) {
+            return await context.prisma.order({ id: parent.id }).user();
+        },
+    },
+    User: {
+        async city(parent,args,{context}) {
+            return await context.prisma.user({ id: parent.id }).city();
+        },
     },
 };
 
